662ecbf33f9f4292a89ba499b50aeb5d1a00c5ae
Sravani Jonuboina <jonuboinasravani777@gmail.com>
2025-08-27 20:28:43 +0530

    Add files via upload

---
+++ datatypes.py
@@ -0,0 +1,14 @@
+a =200
+b=30.56
+c=a+b
+dict= {'name': 'Sharon','age':21}
+set={1,2,3}
+list=[6,7,8]
+bool=("True","False")
+print("this is data types")
+print(c)
+print(type(c))
+print(list)
+print(dict)
+print(set)
+print(bool)
